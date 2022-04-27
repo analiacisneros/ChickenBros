dc46f062daddbc92cbf3e373309dd0f87c5c942d
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-04-27 17:16:23 -0300

    Update Cliente.java

--- src/main/java/com/ChickenBros/Entidades/Cliente.java
+++ src/main/java/com/ChickenBros/Entidades/Cliente.java
@@ -1,7 +1,42 @@
 
 package com.ChickenBros.Entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
 
+@Entity
 public class Cliente {
     
+    	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+        private String idCliente;
+        private String direccion;
+        private Long telefono;
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
+    }
 }
