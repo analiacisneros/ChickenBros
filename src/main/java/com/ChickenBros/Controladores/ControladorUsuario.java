6ad263cb586b383305d4ddf45a96879d4293e0d4
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-04-27 17:20:35 -0300

    Update ControladorUsuario.java

--- src/main/java/com/ChickenBros/Controladores/ControladorUsuario.java
+++ src/main/java/com/ChickenBros/Controladores/ControladorUsuario.java
@@ -1,7 +1,41 @@
 
 package com.ChickenBros.Controladores;
 
+import Servicio.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-public class ControladorUsuario {
-    
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioControlador {
+
+	@Autowired
+	private UsuarioServicio usuarioServ;
+
+
+	@GetMapping("/registro")
+	public String formulario() {
+		return "form-usuario";
+	}
+
+	@PostMapping("/registro")
+	public String guardar(ModelMap modelo, @RequestParam String nombre, @RequestParam String apellido,
+			@RequestParam String email, @RequestParam String clave) {
+
+		try {
+			usuarioServ.guardar(nombre, apellido, email, clave);
+
+			modelo.put("exito", "registro exitoso");
+			return "form-usuario";
+		} catch (Exception e) {
+			modelo.put("error", e.getMessage());
+			return "form-usuario";
+		}
+	}
+
 }
