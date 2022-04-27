6a76700380b4175e39068ab4796fc642781ab72a
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-04-27 17:15:46 -0300

    Update Admin.java

--- src/main/java/com/ChickenBros/Entidades/Admin.java
+++ src/main/java/com/ChickenBros/Entidades/Admin.java
@@ -1,7 +1,25 @@
 
 package com.ChickenBros.Entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
 
+
+@Entity
 public class Admin {
-    
+        
+    	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+        private String idAdmin;
+
+    public String getIdAdmin() {
+        return idAdmin;
+    }
+
+    public void setIdAdmin(String idAdmin) {
+        this.idAdmin = idAdmin;
+    }        
 }
