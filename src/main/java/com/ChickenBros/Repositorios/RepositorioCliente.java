f37034f0b978634284b337e409d227eb22b93bfe
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-04-27 17:18:20 -0300

    Update RepositorioCliente.java

--- src/main/java/com/ChickenBros/Repositorios/RepositorioCliente.java
+++ src/main/java/com/ChickenBros/Repositorios/RepositorioCliente.java
@@ -5,10 +5,18 @@
  */
 package com.ChickenBros.Repositorios;
 
-/**
- *
- * @author Analia
- */
-public interface RepositorioCliente {
-    
+
+import Entidad.Cliente;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+
+@Repository
+public interface ClienteRepositorio extends JpaRepository <Cliente, String>{
+
+	@Query("SELECT c from Cliente c WHERE c.direccion = :direccion ")
+	public List<Cliente> buscarDireccion(@Param("direccion")String direccion);
+	
 }
