140ffd8fe4ec650f59228286c42782ac93d3fbb5
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-04-27 17:18:48 -0300

    Update RepositorioUsuario.java

--- src/main/java/com/ChickenBros/Repositorios/RepositorioUsuario.java
+++ src/main/java/com/ChickenBros/Repositorios/RepositorioUsuario.java
@@ -5,10 +5,16 @@
  */
 package com.ChickenBros.Repositorios;
 
-/**
- *
- * @author Analia
- */
-public interface RepositorioUsuario {
-    
+import Entidad.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UsuarioRepositorio extends JpaRepository<Usuario, String>{
+	
+	@Query("SELECT a from Usuario a WHERE a.email LIKE :email")
+	public Usuario buscarPorEmail(@Param("email") String email);	
+	
+	@Query("SELECT a from Usuario a WHERE a.nombre LIKE :nombre")
+	public Usuario buscarPorNombre(@Param("nombre") String email);	
 }
