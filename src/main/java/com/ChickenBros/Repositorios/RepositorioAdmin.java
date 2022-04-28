b1257ab3664c78df030b6f36f2f53433958c0c64
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-04-28 18:01:19 -0300

    Create RepositorioAdmin.java

---
+++ src/main/java/com/ChickenBros/Repositorios/RepositorioAdmin.java
@@ -0,0 +1,15 @@
+package com.ChickenBros.Repositorios;
+
+import Entidad.Admin;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+
+@Repository
+public interface AdminRepositorio extends JpaRepository <Admin, String>{
+
+	@Query("SELECT a from Admin a WHERE a.idAdmin = :idAdmin ")
+	public List<Admin> buscarId(@Param("idAdmin") String idAdmin);
+}
