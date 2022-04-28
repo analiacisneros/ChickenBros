f15f531c5fb365d0c18dc30362f8ffce175bb22a
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-04-28 17:59:22 -0300

    Create ServicioAdmin.java

---
+++ src/main/java/com/ChickenBros/Servicios/ServicioAdmin.java
@@ -0,0 +1,35 @@
+package com.ChickenBros.Servicios;
+
+import Entidad.Admin;
+import Entidad.Usuario;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import Repositorio.AdminRepositorio;
+
+
+@Service
+public class AdminServicio extends Usuario{
+
+    	@Autowired
+	private AdminRepositorio adminRepo;
+
+	@Transactional
+	public Admin guardar(String id) throws Exception {
+            Admin entidad = new Admin();
+            
+            entidad.setIdAdmin(id);
+            return adminRepo.save(entidad);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Admin> listarId(String idAdmin) {
+            return adminRepo.buscarId(idAdmin);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Admin> listarTodos() {
+            return adminRepo.findAll();
+	}
+}
