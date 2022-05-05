c418c874ddebea1caae98b968798fc7aa39fd5d6
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-05-05 18:48:16 -0300

    Update ServicioAdmin.java

--- src/main/java/com/ChickenBros/Servicios/ServicioAdmin.java
+++ src/main/java/com/ChickenBros/Servicios/ServicioAdmin.java
@@ -2,39 +2,82 @@ package com.ChickenBros.Servicios;
 
 import Entidad.Admin;
 import Entidad.Usuario;
+import Entidad.Enum.Rol;
+import Entidad.Repocitorios.AdminRepositorio;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import Repositorio.AdminRepositorio;
-
 
 @Service
-public class AdminServicio extends Usuario{
+public class AdminServicio implements UserDetailsService {
 
     	@Autowired
 	private AdminRepositorio adminRepo;
 
 	@Transactional
-	public Admin guardar(String id) throws Exception {
-            Admin entidad = new Admin();
-            Admin entidad = new Admin();          
-//            entidad.setIdAdmin(id);
+	public Admin guardar(String nombre, String apellido, String email, String clave) throws Exception {
+            Admin entidad = new Admin();   
+
+		entidad.setNombre(nombre);
+		entidad.setApellido(apellido);
+		entidad.setEmail(email);
+		entidad.setClave(new BCryptPasswordEncoder().encode(clave));
+		entidad.setRol(Rol.ADMIN);
+            
             return adminRepo.save(entidad);
 	}
+        
+        public void modificarAdmin (String id,String nombre, String apellido, String email, String clave) throws Exception {
+                                 
+            Optional<Admin> respuesta = adminRepo.findById(id);
+            if(respuesta.isPresent()){
+            Admin entidad = respuesta.get();
+            
+                entidad.setNombre(nombre);
+		entidad.setApellido(apellido);
+		entidad.setEmail(email);
+		entidad.setClave(new BCryptPasswordEncoder().encode(clave));
 
-//	@Transactional(readOnly = true)
-//	public List<Admin> listarId(String idAdmin) {
-//            return adminRepo.buscarId(idAdmin);
-//	}
+            } else{
+                throw new Error("No se encontro el usuario");
+            }
+        }
+
+	@Transactional(readOnly = true)
+	public List<Admin> listarId(String email) {
+            return adminRepo.buscarEmail(email);
+	}
 
 	@Transactional(readOnly = true)
 	public List<Admin> listarTodos() {
             return adminRepo.findAll();
 	}
-	
-	@Transactional(readOnly = true)
-	public List<Admin> listarTodos() {
-            return adminRepo.findAll();
-	}
+
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        		
+		Admin admin = adminRepo.getById(id);
+		
+		if (admin != null) {
+			List<GrantedAuthority> permisos = new ArrayList<>();
+                        
+			GrantedAuthority p = new SimpleGrantedAuthority("ADMIN");
+			permisos.add(p);
+                        
+                        User cliente = new User(admin.getEmail(), admin.getClave(), permisos);
+                        return cliente;
+		} else{                
+		return null;}
+    }
 }
