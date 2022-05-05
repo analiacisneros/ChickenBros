48e17186d96739d2d09e5e5f73f1c0f5e97ce66c
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-05-05 19:02:57 -0300

    Update RepositorioCliente.java

--- src/main/java/com/ChickenBros/Repositorios/RepositorioCliente.java
+++ src/main/java/com/ChickenBros/Repositorios/RepositorioCliente.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ChickenBros.Repositorios;
 
-
-import Entidad.Cliente;
+import com.ChickenBros.Entidades.Cliente;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
