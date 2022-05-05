fba755ececbabc329e7222dd877113a5fd038031
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-05-05 19:01:54 -0300

    Update RepositorioAdmin.java

--- src/main/java/com/ChickenBros/Repositorios/RepositorioAdmin.java
+++ src/main/java/com/ChickenBros/Repositorios/RepositorioAdmin.java
@@ -1,6 +1,6 @@
 package com.ChickenBros.Repositorios;
 
-import Entidad.Admin;
+import com.ChickenBros.Entidades.Admin;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface AdminRepositorio extends JpaRepository <Admin, String>{
-
-//	@Query("SELECT a from Admin a WHERE a.idAdmin = :idAdmin ")
-//	public List<Admin> buscarId(@Param("idAdmin") String idAdmin);
+  
+	@Query("SELECT a from Admin a WHERE a.email = :email ")
+	public List<Admin> buscarEmail(@Param("email") String idAdmin);
 }
