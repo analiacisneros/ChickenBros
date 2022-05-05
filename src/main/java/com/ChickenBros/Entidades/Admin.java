beb43a2eb52638dbe042e62b3fbf21c7b939b72d
EzequielLuna <96833175+EzeNeri5@users.noreply.github.com>
2022-05-05 18:43:45 -0300

    Update Admin.java

--- src/main/java/com/ChickenBros/Entidades/Admin.java
+++ src/main/java/com/ChickenBros/Entidades/Admin.java
@@ -1,25 +1,64 @@
 
 package com.ChickenBros.Entidades;
 
+import com.Pollo.demo.src.Enum.Rol;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 
 @Entity
-public class Admin {
+public class Admin extends Usuario {
 	
-//    	@Id
-//	@GeneratedValue(generator = "uuid")
-//	@GenericGenerator(name = "uuid", strategy = "uuid2")
-//
+    @Enumerated(EnumType.STRING)
+    private Rol rol;   
 
-//    public String getIdAdmin() {
-//        return idAdmin;
-//    }
-//
-//    public void setIdAdmin(String idAdmin) {
-//        this.idAdmin = idAdmin;
-//    }  
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
 }
+
